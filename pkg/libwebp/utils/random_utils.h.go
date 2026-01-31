320b2b53626d8bc42ecc9f52f0eb63619e732ebc
DaanV2 <2393905+DaanV2@users.noreply.github.com>
2026-01-31 21:36:12 +0100

    chore: cleaning up random_utils

--- pkg/libwebp/utils/random_utils.h.go
+++ pkg/libwebp/utils/random_utils.h.go
@@ -13,44 +13,45 @@ package utils
 //
 // Author: Skal (pascal.massimino@gmail.com)
 
-
 import "github.com/daanv2/go-webp/pkg/assert"
 
-import "github.com/daanv2/go-webp/pkg/libwebp/utils"
-import "github.com/daanv2/go-webp/pkg/libwebp/webp"
 
-
-const VP8_RANDOM_DITHER_FIX =8  // fixed-point precision for dithering
-const VP8_RANDOM_TABLE_SIZE =55
+const VP8_RANDOM_DITHER_FIX = 8  // fixed-point precision for dithering
+const VP8_RANDOM_TABLE_SIZE = 55
 
 type VP8Random struct {
-  int index1, index2;
-  uint32 tab[VP8_RANDOM_TABLE_SIZE];
-  int amp;
-} ;
+  index1, index2 int
+  tab [VP8_RANDOM_TABLE_SIZE]uint32
+  amp int
+}
 
-// Initializes random generator with an amplitude 'dithering' in range [0..1].
-func VP8InitRandom(const rg *VP8Random, float dithering);
 
 // Returns a centered pseudo-random number with 'num_bits' amplitude.
 // (uses D.Knuth's Difference-based random generator).
 // 'amp' is in VP8_RANDOM_DITHER_FIX fixed-point precision.
-static  int VP8RandomBits2(const rg *VP8Random, int num_bits, int amp) {
-  int diff;
-  assert.Assert(num_bits + VP8_RANDOM_DITHER_FIX <= 31);
-  diff = rg.tab[rg.index1] - rg.tab[rg.index2];
-  if (diff < 0) diff += (uint(1) << 31);
-  rg.tab[rg.index1] = diff;
-  if (++rg.index1 == VP8_RANDOM_TABLE_SIZE) rg.index1 = 0;
-  if (++rg.index2 == VP8_RANDOM_TABLE_SIZE) rg.index2 = 0;
+func VP8RandomBits2(/* const  */rg *VP8Random, num_bits, amp int) int {
+  var diff int
+  assert.Assert(num_bits + VP8_RANDOM_DITHER_FIX <= 31)
+
+  diff = rg.tab[rg.index1] - rg.tab[rg.index2]
+  if (diff < 0) {diff += (uint(1) << 31)}
+  rg.tab[rg.index1] = diff
+  if (rg.index1 == VP8_RANDOM_TABLE_SIZE) {rg.index1 = 0} else {
+	rg.index1++
+  }
+  if (rg.index2 == VP8_RANDOM_TABLE_SIZE) {rg.index2 = 0} else {
+	rg.index2++
+  }
+  
+ 
   // sign-extend, 0-center
-  diff = (int)((uint32)diff << 1) >> (32 - num_bits);
-  diff = (diff * amp) >> VP8_RANDOM_DITHER_FIX;  // restrict range
-  diff += 1 << (num_bits - 1);                   // shift back to 0.5-center
-  return diff;
+  diff = (diff << 1) >> (32 - num_bits)
+  diff = (diff * amp) >> VP8_RANDOM_DITHER_FIX  // restrict range
+  diff += 1 << (num_bits - 1)                   // shift back to 0.5-center
+  return diff
 }
 
-static  int VP8RandomBits(const rg *VP8Random, int num_bits) {
-  return VP8RandomBits2(rg, num_bits, rg.amp);
+func VP8RandomBits(/* const  */rg *VP8Random, num_bits int) int {
+  return VP8RandomBits2(rg, num_bits, rg.amp)
 }
 
