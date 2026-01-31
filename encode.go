eae8659d9768ae4767bcb8cbe9b4ea8a6af28fbf
DaanV2 <2393905+DaanV2@users.noreply.github.com>
2026-01-31 10:33:49 +0100

    tmp: setup

---
+++ encode.go
@@ -0,0 +1,32 @@
+package webp
+
+import (
+	"errors"
+	"image"
+	"io"
+
+	"github.com/daanv2/go-webp/pkg/config"
+	"github.com/daanv2/go-webp/pkg/encodings/lossless"
+	"github.com/daanv2/go-webp/pkg/encodings/lossy"
+)
+
+func Encode(w io.Writer, img image.Image, options *config.Config) error {
+	if options == nil {
+		return errors.New("options is nil")
+	}
+	if img == nil {
+		return errors.New("img is nil")
+	}
+	if w == nil {
+		return errors.New("writer is nil")
+	}
+
+	switch options.LossLess {
+	case 1: // Lossless
+		return lossless.Encode(w, img, options)
+	case 0: // Lossy
+		return lossy.Encode(w, img, options)
+	default:
+		return errors.New("invalid lossless option, must be 0 (lossy) or 1 (lossless)")
+	}
+}
