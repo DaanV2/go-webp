fa8017b93fdbe8b51a7a0aa740893cbb31f61ab7
DaanV2 <2393905+DaanV2@users.noreply.github.com>
2026-02-05 20:13:50 +0100

    removing header file

--- pkg/vp8/color_cache.go
+++ pkg/vp8/color_cache.go
@@ -1,5 +1,3 @@
-package utils
-
 // Copyright 2012 Google Inc. All Rights Reserved.
 //
 // Use of this source code is governed by a BSD-style license
@@ -7,11 +5,9 @@ package utils
 // tree. An additional intellectual property rights grant can be found
 // in the file PATENTS. All contributing project authors may
 // be found in the AUTHORS file in the root of the source tree.
-// -----------------------------------------------------------------------------
-//
-// Color Cache for WebP Lossless
-//
-// Author: Jyrki Alakuijala (jyrki@google.com)
+
+package vp8
+
 
 import "github.com/daanv2/go-webp/pkg/libwebp/utils"
 
@@ -23,10 +19,10 @@ import "github.com/daanv2/go-webp/pkg/libwebp/utils"
 import "github.com/daanv2/go-webp/pkg/libwebp/utils"
 import "github.com/daanv2/go-webp/pkg/libwebp/webp"
 
+const kHashMul  = uint32(0x1e35a7bd);
 
-//------------------------------------------------------------------------------
-// VP8LColorCache.
-
+// Initializes the color cache with 'hash_bits' bits for the keys.
+// Returns false in case of memory error.
 func VP8LColorCacheInit(/* const */ color_cache *VP8LColorCache, int hash_bits) int {
   hash_size := 1 << hash_bits;
   colors *uint32 = (*uint32)WebPSafeCalloc((uint64)hash_size, sizeof(*color_cache.colors));
@@ -44,11 +40,41 @@ func VP8LColorCacheInit(/* const */ color_cache *VP8LColorCache, int hash_bits) 
   return 1;
 }
 
+// Delete the memory associated to color cache.
 func VP8LColorCacheClear(/* const */ color_cache *VP8LColorCache) {
   if (color_cache != nil) {
     color_cache.colors = nil;
     WEBP_SELF_ASSIGN(color_cache.hash_bits);
   }
+}
+
+func VP8LHashPix(argb uint32, int shift) int {
+  return (int)((argb * kHashMul) >> shift);
+}
+
+func VP8LColorCacheLookup(/* const */ cc *VP8LColorCache, key uint32) uint32 {
+  assert.Assert((key >> cc.hash_bits) == uint(0));
+  return cc.colors[key];
+}
+
+func VP8LColorCacheSet(/* const */ cc *VP8LColorCache, key uint32, argb uint32) {
+  assert.Assert((key >> cc.hash_bits) == uint(0));
+  cc.colors[key] = argb;
+}
+
+func VP8LColorCacheInsert(/* const */ cc *VP8LColorCache, argb uint32) {
+  key := VP8LHashPix(argb, cc.hash_shift);
+  cc.colors[key] = argb;
+}
+
+func VP8LColorCacheGetIndex(/* const */ cc *VP8LColorCache, argb uint32) int {
+  return VP8LHashPix(argb, cc.hash_shift);
+}
+
+// Return the key if cc contains argb, and -1 otherwise.
+func VP8LColorCacheContains(/* const */ cc *VP8LColorCache, argb uint32) int {
+  key := VP8LHashPix(argb, cc.hash_shift);
+  return tenary.If(cc.colors[key] == argb, key, -1)
 }
 
 func VP8LColorCacheCopy(/* const */ src *VP8LColorCache, /*const*/ dst *VP8LColorCache) {
