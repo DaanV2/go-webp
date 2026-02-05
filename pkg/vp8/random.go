1c892226befb78d6f124ee2dc8744c4d76561e78
DaanV2 <2393905+DaanV2@users.noreply.github.com>
2026-02-05 20:14:59 +0100

    cleanup function files

--- pkg/vp8/random.go
+++ pkg/vp8/random.go
@@ -6,16 +6,16 @@
 // in the file PATENTS. All contributing project authors may
 // be found in the AUTHORS file in the root of the source tree.
 
-package utils
+package vp8
 
 import "github.com/daanv2/go-webp/pkg/assert"
 
-const VP8_RANDOM_DITHER_FIX = 8  // fixed-point precision for dithering
+const VP8_RANDOM_DITHER_FIX = 8 // fixed-point precision for dithering
 const VP8_RANDOM_TABLE_SIZE = 55
 
 // 31b-range values
 var kRandomTable = [VP8_RANDOM_TABLE_SIZE]uint32{
-    0x0de15230, 0x03b31886, 0x775faccb, 0x1c88626a,
+	0x0de15230, 0x03b31886, 0x775faccb, 0x1c88626a,
 	0x68385c55, 0x14b3b828, 0x4a85fef8, 0x49ddb84b,
 	0x64fcf397, 0x5c550289, 0x4a290000, 0x0d7ec1da,
 	0x5940b7ab, 0x5492577d, 0x4e19ca72, 0x38d38c69,
@@ -32,53 +32,57 @@ var kRandomTable = [VP8_RANDOM_TABLE_SIZE]uint32{
 }
 
 type VP8Random struct {
-  index1, index2 int
-  tab [VP8_RANDOM_TABLE_SIZE]uint32
-  amp int
+	index1, index2 int
+	tab            [VP8_RANDOM_TABLE_SIZE]uint32
+	amp            int
 }
 
-
 // VP8InitRandom initializes random generator with an amplitude 'dithering' in range [0..1].
-func VP8InitRandom(/* const */ rg *VP8Random, dithering float64) {
+func VP8InitRandom( /* const */ rg *VP8Random, dithering float64) {
 	copy(rg.tab, kRandomTable)
 
-	rg.index1 = 0;
-	rg.index2 = 31;
+	rg.index1 = 0
+	rg.index2 = 31
 
-	if (dithering < 0.0) {
-		rg.amp = 0;
-	} else if (dithering > 1.0) {
-		rg.amp = (1 << VP8_RANDOM_DITHER_FIX);
+	if dithering < 0.0 {
+		rg.amp = 0
+	} else if dithering > 1.0 {
+		rg.amp = (1 << VP8_RANDOM_DITHER_FIX)
 	} else {
-		rg.amp = (uint32)((1 << VP8_RANDOM_DITHER_FIX) * dithering);
+		rg.amp = (uint32)((1 << VP8_RANDOM_DITHER_FIX) * dithering)
 	}
 }
 
 // Returns a centered pseudo-random number with 'num_bits' amplitude.
 // (uses D.Knuth's Difference-based random generator).
 // 'amp' is in VP8_RANDOM_DITHER_FIX fixed-point precision.
-func VP8RandomBits2(/* const  */rg *VP8Random, num_bits, amp int) int {
-  var diff int
-  assert.Assert(num_bits + VP8_RANDOM_DITHER_FIX <= 31)
+func VP8RandomBits2( /* const  */ rg *VP8Random, num_bits, amp int) int {
+	var diff int
+	assert.Assert(num_bits+VP8_RANDOM_DITHER_FIX <= 31)
 
-  diff = rg.tab[rg.index1] - rg.tab[rg.index2]
-  if (diff < 0) {diff += (uint(1) << 31)}
-  rg.tab[rg.index1] = diff
-  if (rg.index1 == VP8_RANDOM_TABLE_SIZE) {rg.index1 = 0} else {
-	rg.index1++
-  }
-  if (rg.index2 == VP8_RANDOM_TABLE_SIZE) {rg.index2 = 0} else {
-	rg.index2++
-  }
-  
- 
-  // sign-extend, 0-center
-  diff = (diff << 1) >> (32 - num_bits)
-  diff = (diff * amp) >> VP8_RANDOM_DITHER_FIX  // restrict range
-  diff += 1 << (num_bits - 1)                   // shift back to 0.5-center
-  return diff
+	diff = rg.tab[rg.index1] - rg.tab[rg.index2]
+	if diff < 0 {
+		diff += (uint(1) << 31)
+	}
+	rg.tab[rg.index1] = diff
+	if rg.index1 == VP8_RANDOM_TABLE_SIZE {
+		rg.index1 = 0
+	} else {
+		rg.index1++
+	}
+	if rg.index2 == VP8_RANDOM_TABLE_SIZE {
+		rg.index2 = 0
+	} else {
+		rg.index2++
+	}
+
+	// sign-extend, 0-center
+	diff = (diff << 1) >> (32 - num_bits)
+	diff = (diff * amp) >> VP8_RANDOM_DITHER_FIX // restrict range
+	diff += 1 << (num_bits - 1)                  // shift back to 0.5-center
+	return diff
 }
 
-func VP8RandomBits(/* const  */rg *VP8Random, num_bits int) int {
-  return VP8RandomBits2(rg, num_bits, rg.amp)
+func VP8RandomBits( /* const  */ rg *VP8Random, num_bits int) int {
+	return VP8RandomBits2(rg, num_bits, rg.amp)
 }
