539dea6bdf3652dceb91a043640f28a659d80e2c
DaanV2 <2393905+DaanV2@users.noreply.github.com>
2026-02-07 12:40:54 +0100

    fix: simplifying opaque

--- pkg/color/colorspace/alpha/opague.go
+++ pkg/color/colorspace/alpha/opague.go
@@ -3,41 +3,41 @@ package alpha
 import "image/color"
 
 // Returns true if alpha[] has non-0xff values.
-func CheckNonOpaque(/* const */ alpha []color.Alpha, width, height int, x_step int, y_step int) int {
-  if alpha == nil { return 0  }
-  WebPInitAlphaProcessing();
-  if x_step == 1 {
-    for i := 0; i < height; i++ {
-      if WebPHasAlpha8b(alpha, width) { return 1 }
-      alpha += y_step
-    }
-  } else {
-    for i := 0; i < height; i++ {
-      if WebPHasAlpha32b(alpha, width) { return 1 }
-      alpha += y_step
-    }
-  }
-  return 0;
+func CheckNonOpaque( /* const */ alpha []color.Alpha) bool {
+	for _, i := range alpha {
+		if uint8(i.A) != 0xff {
+			return true
+		}
+	}
+
+	return false
 }
 
-func HasAlpha8b_C(/* const */ src *uint8, length int) int {
-  while (length-- > 0) {
-    if *src++ != 0xff { return 1  }
-  }
-  return 0;
+// Returns true if alpha[] has non-0xff values.
+func CheckNonOpaque2[T color.Color]( /* const */ clrs []T) bool {
+	for _, i := range clrs {
+		_, _, _, a := i.RGBA()
+		if uint8(a) != 0xff {
+			return true
+		}
+	}
+
+	return false
 }
 
-func HasAlpha32b_C(/* const */ src *uint8, length int) int {
-  var x int
-  for x = 0; length-- > 0; x += 4 {
-    if src[x] != 0xff { return 1  }
-  }
-  return 0;
+func WebPHasAlpha8b(src []color.Alpha) bool {
+	for _, i := range src {
+		if uint8(i.A) != 0xff {
+			return true
+		}
+	}
+	return false
 }
 
-func AlphaReplace_C(src *uint32, length int, color uint32) {
-  var x int
-  for x = 0; x < length; x++ {
-    if (src[x] >> 24) == 0 { src[x] = color }
-  }
+func WebPAlphaReplace(src []color.Alpha, length int, color uint8) {
+	for x := range length {
+		if src[x].A == 0 {
+			src[x].A = color
+		}
+	}
 }
