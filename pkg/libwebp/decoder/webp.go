c63b3f46ac97b8c05391f2354e2260699d79375d
DaanV2 <2393905+DaanV2@users.noreply.github.com>
2026-02-09 21:17:32 +0100

    formatted package

--- pkg/libwebp/decoder/webp.go
+++ pkg/libwebp/decoder/webp.go
@@ -1,5 +1,3 @@
-package decoder
-
 // Copyright 2010 Google Inc. All Rights Reserved.
 //
 // Use of this source code is governed by a BSD-style license
@@ -7,6 +5,8 @@ package decoder
 // tree. An additional intellectual property rights grant can be found
 // in the file PATENTS. All contributing project authors may
 // be found in the AUTHORS file in the root of the source tree.
+
+package decoder
 
 import (
 	"github.com/daanv2/go-webp/pkg/assert"
